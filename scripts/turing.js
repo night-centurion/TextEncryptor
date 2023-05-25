14c937d5215f4c9116b9786cddb4100dcbb9021d
miguel-oficina <miguel_campiz@hotmail.com>
2023-05-25 16:06:59 -0500

    new files

---
+++ scripts/turing.js
@@ -0,0 +1,6 @@
+let saludo = 'Hola mundo'
+saludo.replace('mundo','EDteam')
+console.log(saludo) // 'Hola mundo' (no se modifica el string original)
+
+// let saludo2 = saludo.replace('mundo','EDteam')
+// console.log(saludo2) //
